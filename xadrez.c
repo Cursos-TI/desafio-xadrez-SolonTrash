51b3b3d936a5c424cdce44cef22cea6780616477
SolonTrash <solontrash@gmail.com>
2025-03-03 13:37:32 +0000

    Xadrez - final

--- xadrez.c
+++ xadrez.c
@@ -1,43 +1,61 @@
 #include <stdio.h>
 
+// Recursiva Torre
+void movimentoTorre(int casas) {
+    if (casas == 0) {
+        return;
+    }
+    printf("Casa %d: Direita\n", 6 - casas); // Contagem decrescente
+    movimentoTorre(casas - 1);
+}
+
+//Recurssiva com Loops aninhados para o Bispo
+void movimentoBispo(int casas) {
+    if (casas == 0) {
+        return;
+    }
+    printf("Casa %d: cima, Direita\n", 6 - casas); 
+    movimentoBispo(casas - 1);
+}
+
+// Recursiva para a Rainha
+void movimentoRainha(int casas) {
+    if (casas == 0) {
+        return;
+    }
+    printf("Casa %d: Esquerda\n", 9 - casas); // Contagem decrescente
+    movimentoRainha(casas - 1);
+}
+
 int main() {
     // Movimento da Torre (5 casas para direita)
     printf("Movimento da Torre:\n");
-    for (int i = 1; i <= 5; i++) {
-        printf("Casa %d: Direita\n", i);
-    }
+    movimentoTorre(5);
 
     // Movimento do Bispo (5 casas na diagonal para cima e a direita)
     printf("\nMovimento do Bispo:\n");
-    int i = 1;
-    while (i <= 5) {
-        printf("Casa %d: Cima, Direita\n", i);
-        i++;
-    }
+    movimentoBispo(5);
 
     // Movimento da Rainha (8 casas para esquerda)
-    printf("\nMovimento do Bispo:\n");
-    int j = 1;
-    do {
-       printf("Casa %d: Esquerda\n", j);
-        j++;
-    } while (j<= 8);
+    printf("\nMovimento da Rainha:\n");
+    movimentoRainha(8);
 
-    //Movimento do Cavalo (2 para baixo, 1 para esquerda)
+    //Movimento do Cavalo (2 para cima e 1 para a direita, em "L")
     printf("\nMovimento do Cavalo:\n");
-    int passosParaBaixo = 2; // Número de casas para baixo
-    int passosParaEsquerda = 1; // Número de casas para esquerda
+    int passosParaCima = 2; 
+    int passosParaDireita = 1; 
 
-    // Loop for para o movimento vertical - para baixo
-    for (int k = 1; k <= passosParaBaixo; k++) {
-        printf("Casa %d: Baixo\n", k);
-    }
-
-    // Loop while para o movimento horizontal (para a esquerda)
-    int l = 1;
-    while (l <= passosParaEsquerda) {
-        printf("Casa %d: Esquerda\n", passosParaBaixo + l);
-        l++;
+    // Loops complexos para o movimento vertical - para baixo
+    for (int i = 1; i <= passosParaCima; i++) {
+        printf("Casa %d: Cima\n", i);
+        if (i == passosParaCima) {
+            for (int j = 1; j <= passosParaDireita; j++) {
+                if (j == passosParaDireita) {
+                    printf("Casa %d: Direita\n", passosParaCima + j);
+                    break; 
+                }
+            }
+        }
     }
 
     return 0;
